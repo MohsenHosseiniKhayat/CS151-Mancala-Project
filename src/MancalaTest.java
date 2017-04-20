68af797591bfae2655ee4024cd9969f59744cca4
Nithya Rajan <nithyar2005@gmail.com>
2017-04-20 15:50:53 -0700

    modified MancalaPit

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -1,18 +1,8 @@
 
-import javax.swing.JOptionPane;
-
-
 public class MancalaTest {
 
     public static void main(String[] args) {
   
-  //String[] styles = {"BLACK AND WHITE STYLE "," COLORFUL STYLE"};
-  //int selected = JOptionPane.showOptionDialog(null, "Choose your mancala style: ", null, JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
-  
-    String[] options = {"4","3"};
-    int selected = JOptionPane.showOptionDialog(null, "Choose the number of stones in each pit: ", null, JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
-    int numberOfStones = Integer.parseInt(options[selected]);
-    
-    MancalaModel m = new MancalaModel(numberOfStones);
+
     }
 }

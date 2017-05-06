c84eabecbab80d688856f7c5e5ecb707ab234e20
Nithya Rajan <nithyar2005@gmail.com>
2017-05-06 12:39:17 -0700

    added user's choice of stones

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -1,16 +1,21 @@
-import sun.java2d.pipe.SpanShapeRenderer;
+import javax.swing.JOptionPane;
 
-public class MancalaTest
-{
-    public static void main(String[] args)
-    {
-       MancalaModel model = new MancalaModel(4);
-       System.out.println(model.getEndOfTurn());
-       model.takeTurn(model.getCurrentPlayer(), 1,3);
-       System.out.println(model.getEndOfTurn());
-       model.switchPlayer();
-       System.out.println(model.getEndOfTurn());
-       model.takeTurn(model.getCurrentPlayer(),0,2);
-       System.out.println(model.getEndOfTurn());
-    }
+public class MancalaTest {
+	public static void main(String[] args) {
+		
+		String[] options = { "4", "3" };
+		int selected = JOptionPane.showOptionDialog(null, "Choose the number of stones in each pit: ", null,
+				JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+		int numberOfStones = Integer.parseInt(options[selected]);
+
+		MancalaModel model = new MancalaModel(numberOfStones);
+
+		System.out.println(model.getEndOfTurn());
+		model.takeTurn(model.getCurrentPlayer(), 1, 3);
+		System.out.println(model.getEndOfTurn());
+		model.switchPlayer();
+		System.out.println(model.getEndOfTurn());
+		model.takeTurn(model.getCurrentPlayer(), 0, 2);
+		System.out.println(model.getEndOfTurn());
+	}
 }
